a06815d0998c87b4af8db458d8cf900aea9ce0d3
Łukasz <llach@llach.pl>
2019-09-25 21:37:13 +0200

    task: v19.09; task: publish tunneled 6080

--- .workshop/tunnel.sh
+++ .workshop/tunnel.sh
@@ -21,11 +21,12 @@ docker rm -f orca-tunnel &>/dev/null || true
 docker run -d --name orca-tunnel --restart always \
     -p 4040:4040 \
     -p 5000:5000 \
+    -p 6080:6080 \
     -p 8000:8000 -p 8080:8080 -p 8081:8081 -p 8443:8443 \
     -p 9000:9000 \
     -p 10080:10080 \
     -e SSH_USER=d -e SSH_PASSWORD=docker -e SSH_PORT=80 \
-    lukaszlach/orca-tunnel "vps$VPS_ID.cmd.cat" 4040 5000 8000 8080 8081 8443 9000 10080
+    lukaszlach/orca-tunnel "vps$VPS_ID.cmd.cat" 4040 5000 6080 8000 8080 8081 8443 9000 10080
 
 echo
 echo "Orca Tunnel up and running"
